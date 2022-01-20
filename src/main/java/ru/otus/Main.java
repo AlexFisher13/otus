03900f57fe60903c94bcd84c1404864bd50c20bd
alexfisher13 <alexfisher13@yandex.ru>
2022-01-20 16:06:36 +0300

    refactor to javaBased context

--- src/main/java/ru/otus/Main.java
+++ src/main/java/ru/otus/Main.java
@@ -1,12 +1,14 @@
 package ru.otus;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import ru.otus.domain.Person;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.otus.service.PersonService;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         PersonService service = context.getBean(PersonService.class);
         Person ivan = service.getByName("Ivan");
         System.out.println(ivan);
