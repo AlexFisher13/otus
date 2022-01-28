ed8f099273311f520adb418c5a353eae9b5509f5
Fisher <alexfisher13@yandex.ru>
2022-01-29 00:16:16 +0300

    move to SpringBoot

--- src/main/java/ru/otus/Application.java
+++ src/main/java/ru/otus/Application.java
@@ -1,21 +1,16 @@
 package ru.otus;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import ru.otus.domain.Person;
 import ru.otus.service.PersonService;
 
 @EnableAspectJAutoProxy //сканирует наличие компонентов помеченных @Aspect
-@ComponentScan
-@Configuration
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(Application.class);
-
-
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         PersonService service = context.getBean(PersonService.class);
         Person ivan = service.getByName("Ivan");
         System.out.println(ivan);
